35b394bf6fa7deb4fdc559c767d6c4e3309ac0aa
Avishek Amin <avishekamin207@gmail.com>
2026-09-09 04:23:24 +0530

    feat: add video meeting lobby

---
+++ frontend/src/pages/videoMeet.jsx
@@ -0,0 +1,144 @@
+import React, { useEffect, useRef, useState } from "react";
+import { TextField, Button } from "@mui/material";
+import "../styles/videoComponent.css";
+
+const server_url = "http://localhost:8080";
+var connections = {};
+
+const peerConfigConnections = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+};
+
+export default function VideoMeetComponent() {
+  var socketRef = useRef();
+  let socketIdRef = useRef();
+  let localVideoRef = useRef();
+  let [videoAvailable, setVideoAvailable] = useState(true);
+  let [audioAvailable, setAudioAvailable] = useState(true);
+  let [screenAvailable, setScreenAvailable] = useState();
+  let [video, setVideo] = useState();
+  let [audio, setAudio] = useState();
+  let [screen, setScreen] = useState();
+  let [showModal, setModal] = useState();
+  let [message, setMessage] = useState("");
+  let [messages, setMessages] = useState([]);
+  let [newMessages, setNewMessages] = useState(0);
+  let [askForUsername, setAskForUsername] = useState(true);
+  let [username, setUsername] = useState("");
+  const videoRef = useRef([]);
+  let [videos, setVideos] = useState([]);
+
+  const getPermissions = async () => {
+    try {
+      const videoPermission = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+
+      if (videoPermission) {
+        setVideoAvailable(true);
+      } else {
+        setVideoAvailable(false);
+      }
+
+      const audioPermission = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+
+      if (audioPermission) {
+        setAudioAvailable(true);
+      } else {
+        setAudioAvailable(false);
+      }
+
+      if (navigator.mediaDevices.getDisplayMedia) {
+        setScreenAvailable(true);
+      } else {
+        setScreenAvailable(false);
+      }
+
+      if (videoAvailable || audioAvailable) {
+        const userMediaStream = await navigator.mediaDevices.getUserMedia({
+          video: videoAvailable,
+          audio: audioAvailable,
+        });
+
+        if (userMediaStream) {
+          window.localStream = userMediaStream;
+          if (localVideoRef.current) {
+            localVideoRef.current.srcObject = userMediaStream;
+          }
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getPermissions();
+  }, []);
+
+  let getUserMediaSuccess = (stream) => {};
+
+  let getUserMedia = () => {
+    if ((video && videoAvailable) || (audio && audioAvailable)) {
+      navigator.mediaDevices
+        .getUserMedia({ video: video, audio: audio })
+        .then((getUserMediaSuccess) => {})
+        .then((stream) => {})
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      try {
+        let tracks = localVideoRef.current.srcObject.getTracks();
+        tracks.forEach((track) => track.stop());
+      } catch (err) {}
+    }
+  };
+
+  useEffect(() => {
+    if (video !== undefined && audio !== undefined) {
+      getUserMedia();
+    }
+  }, [audio, video]);
+
+  let getMedia = () => {
+    setVideo(videoAvailable);
+    setAudio(audioAvailable);
+    // connectToSocketServer();
+  };
+
+  const connect = () => {
+    console.log("Connect button clicked");
+  };
+
+  return (
+    <div>
+      {askForUsername === true ? (
+        <div>
+          <h2>Enter into Lobby</h2>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            variant="outlined"
+          />
+          <Button variant="contained" onClick={connect}>
+            Connekt
+          </Button>
+
+          <div>
+            <video ref={localVideoRef} autoPlay muted></video>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+}
