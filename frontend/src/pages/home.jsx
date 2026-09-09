7c0fd0eb08bd31d11fc2f82f59efa45d2e70fdaa
Avishek Amin <avishekamin207@gmail.com>
2026-09-10 02:16:24 +0530

    feat: implement home page

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import withAuth from "../utils/withAuth";
+import { useNavigate } from "react-router-dom";
+import RestoreIcon from "@mui/icons-material/Restore";
+import { Button, TextField, IconButton } from "@mui/material";
+import "../App.css";
+
+function HomeComponent() {
+  let navigate = useNavigate();
+  const [meetingCode, setMeetingCode] = useState("");
+  let handleJoinVideoCall = async () => {
+    navigate(`/${meetingCode}`);
+  };
+
+  return (
+    <div>
+      <div className="navBar">
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <h2>Connekt</h2>
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <IconButton>
+            <RestoreIcon />
+          </IconButton>
+          <p>History</p>
+          <Button
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/auth");
+            }}
+          >
+            Logout
+          </Button>
+        </div>
+      </div>
+      <div className="meetContainer">
+        <div className="leftPanel">
+          <div>
+            <h2>Providing Quality Video Call Just Like Quality Education</h2>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <TextField
+                onChange={(element) => setMeetingCode(element.target.value)}
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+              <Button onClick={handleJoinVideoCall} variant="contained">
+                Join
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="rightPanel">
+          <img srcSet="/logo.png" alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default withAuth(HomeComponent);
